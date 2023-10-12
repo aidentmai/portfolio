06227e0d60c6ac5b91a07b6bbc0a5513afc38135
Shutra <aidentmai3@gmail.com>
2023-10-12 01:11:16 -0700

    portfolio-fixed-hamburger-menu

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
+    // Add or remove the 'closed' class depending on the menu state
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('closed');
+    } else {
+        menu.classList.add('closed');
+    }
     icon.classList.toggle("open")
 }
